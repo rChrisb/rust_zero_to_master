23844e7c3718e8e789a81bf3326f9e4b767aafa3
rChrisb <5767@holbertonstudents.com>
2023-01-13 21:10:17 +0100

    Function activity

---
+++ activities/src/bin/a1.rs
@@ -0,0 +1,30 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+fn main() {
+	// println!("My first name is");
+	// displayFirstname("Chris");
+	// println!("My last name is");
+	// displayLasttname("Bikoy");
+	// }
+	// fn displayFirstname(firstName: String){
+	// 	println!({"firstName"})
+	// }
+	// fn displayLastname(lastName: String){
+	// 	println!({"lastName"})
+	first_name();
+	last_name();
+	}
+	fn first_name(){
+		println!("Chris")
+	}
+	fn last_name(){
+		println!("BIKOY")
+	}
---
+++ activities/target/debug/activities.d
@@ -0,0 +1,1 @@
+/home/chrisbikoy/rust_zero_to_master/activities/target/debug/activities: /home/chrisbikoy/rust_zero_to_master/activities/src/main.rs
