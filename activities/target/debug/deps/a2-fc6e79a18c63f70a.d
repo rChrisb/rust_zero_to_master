c861cd0d743c6438c98a6f65a2dd15945d77c4a5
rChrisb <5767@holbertonstudents.com>
2023-01-13 21:27:55 +0100

    Acitivity 2

---
+++ activities/target/debug/deps/a2-fc6e79a18c63f70a.d
@@ -0,0 +1,5 @@
+/home/chrisbikoy/rust_zero_to_master/activities/target/debug/deps/a2-fc6e79a18c63f70a: src/bin/a2.rs
+
+/home/chrisbikoy/rust_zero_to_master/activities/target/debug/deps/a2-fc6e79a18c63f70a.d: src/bin/a2.rs
+
+src/bin/a2.rs:
