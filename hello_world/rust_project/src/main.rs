96b6566055e93b54aa91c459756b523b33c8d365
rChrisb <5767@holbertonstudents.com>
2023-01-13 19:46:37 +0100

    'adding' function and println!

--- hello_world/rust_project/src/main.rs
+++ hello_world/rust_project/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+fn my_first_function(a: i32, b: i32) -> i32 {
+    a + b
+}
+fn main()
+{
+    let mut add = my_first_function(15, 7);
+    println!("{}", add);
+    add = my_first_function(1,5);
+    println!("{}", add);
 }
