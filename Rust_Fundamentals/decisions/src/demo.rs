bd16d18d9a26f1491d11666088ed2ea5d0ad479c
rChrisb <5767@holbertonstudents.com>
2023-01-18 17:47:32 +0100

    match decision

--- Rust_Fundamentals/decisions/src/demo.rs
+++ Rust_Fundamentals/decisions/src/demo.rs
@@ -1,9 +1,24 @@
 fn main()
 {
-	let age = 15;
-	if age <= 15 {
-		println!("You are very young!")
-	} else{
-		println!("You are above 16 years old")
+	let is_major = true;
+	let age = 11;
+	// if age <= 15 {
+	// 	println!("You are very young!")
+	// } else{
+	// 	println!("You are above 16 years old")
+	// }
+	match is_major {
+		true => println!("You are major!"),
+		_ => println!("You are under age!"),
+		// 16 => println!("You are 16!"),
+		// 17 => println!("You are 17!"),  MATCH ONLY WOR
+	}
+	match age {
+		/* true => println!("You are major!"),
+		false => println!("You are under age!"), */
+		16 => println!("You are 16!"),
+		17 => println!("You are 17!"), 
+		18 => println!("You are 18"),
+		_ => return
 	}
 }
