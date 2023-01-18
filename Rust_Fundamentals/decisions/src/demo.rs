8e46f51d33721b163e71ac87db6743b5ad310803
rChrisb <5767@holbertonstudents.com>
2023-01-18 17:56:41 +0100

    match decision

--- Rust_Fundamentals/decisions/src/demo.rs
+++ Rust_Fundamentals/decisions/src/demo.rs
@@ -2,6 +2,7 @@ fn main()
 {
 	let is_major = true;
 	let age = 11;
+	let name = "Lelouch Lamperouge";
 	// if age <= 15 {
 	// 	println!("You are very young!")
 	// } else{
@@ -11,7 +12,7 @@ fn main()
 		true => println!("You are major!"),
 		_ => println!("You are under age!"),
 		// 16 => println!("You are 16!"),
-		// 17 => println!("You are 17!"),  MATCH ONLY WOR
+		// 17 => println!("You are 17!"), 
 	}
 	match age {
 		/* true => println!("You are major!"),
@@ -19,6 +20,12 @@ fn main()
 		16 => println!("You are 16!"),
 		17 => println!("You are 17!"), 
 		18 => println!("You are 18"),
-		_ => return
+		_ => println!("You are {}", age)
+	}
+	match name {
+		"CHRIS" => println!("Hello {}", name),
+		"BIKOY" => println!("Heyy BIKOY"),
+		"RUBIE" => println!("Hi RUBIE"),
+		_ => println!("Goodmorning {}", name),
 	}
 }
