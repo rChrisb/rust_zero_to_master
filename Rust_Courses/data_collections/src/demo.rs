803ab161dfbe7b75085ddf0e7cf39e4adfd35b5e
rChrisb <5767@holbertonstudents.com>
2023-01-19 20:38:36 +0100

    working on implementations of functions

---
+++ Rust_Courses/data_collections/src/demo.rs
@@ -0,0 +1,26 @@
+// struct Temperature {
+// 	degree: f64,
+// }
+// impl Temperature {
+// 	fn freezing()-> Self {
+// 		Self {
+// 			degree: -32.6
+// 		}
+// 	}
+
+// 	fn show_temperature(&self){
+// 		println!("It's {} degree F outside", self.degree);
+// 	}
+	
+// }
+fn main () {
+	
+
+
+
+	// let temp = Temperature {
+	// 	degree : 77.8
+	// };
+	// temp.show_temperature();
+	// let another_temp = Temperature::freezing();
+}
