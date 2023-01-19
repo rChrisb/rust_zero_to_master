528c3c16d181e0a39c29d911b102224d48f94221
rChrisb <5767@holbertonstudents.com>
2023-01-19 15:21:27 +0100

    Data management using tuples

---
+++ activities_Rust/src/bin/a9.rs
@@ -0,0 +1,28 @@
+// Topic: Data management using tuples
+//
+// Requirements:
+// * Print whether the y-value of a cartesian coordinate is
+//   greater than 5, less than 5, or equal to 5
+//
+// Notes:
+// * Use a function that returns a tuple
+// * Destructure the return value into two variables
+// * Use an if..else if..else block to determine what to print
+
+fn main() {
+	let (x, y) = cordination();
+
+	if y > 5 {
+		println!("y-value of catersian coordinate is greater than 5");
+	}
+	else if y < 5 {
+		println!("y-value of catersian coordinate is less than 5");
+	} else {
+
+		println!("y-value of catersian coordinate is equal to 5");
+	}
+
+}
+fn cordination() -> (i32, i32) {
+	return (10, 2);
+}
