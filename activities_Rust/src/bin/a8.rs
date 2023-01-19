20781945c7ea71519902a4a81192edd474401e70
rChrisb <5767@holbertonstudents.com>
2023-01-19 14:49:03 +0100

    Organizing similar data using structs

--- activities_Rust/src/bin/a8.rs
+++ activities_Rust/src/bin/a8.rs
@@ -9,55 +9,55 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-// #[derive(Debug)]
-// enum Flavors {
-// 	Chocolate,
-// 	Apple,
-// 	Orange
-// }
-// struct Drinks {
-// 	flavor: Flavors,
-// 	ounce: f64
-// }
-// fn main() {
-// 	let my_drink = Drinks {
-// 		flavor: Flavors::Apple,
-// 		ounce: 4.2
-// 	};
-
-// 	match my_drink.flavor {
-// 		Flavors::Chocolate => println!("Chocolate drink"),
-// 		Flavors::Apple => println!("Apple juice"),
-// 		Flavors::Orange => println!("Orange juice"),
-// 	}
-// 	print_drink(my_drink);
-
-// }
-// fn print_drink(drink: Drinks){
-// 	println!("the flavor is {:?}", drink.flavor );
-// 	println!("the ounce is {}", drink.ounce);
-// }
 #[derive(Debug)]
-enum Name {
-	Messi,
-	Maradona
+enum Flavors {
+	Chocolate,
+	Apple,
+	Orange
 }
-struct Player {
-	name: Name,
-	ballon_dors: i32
-}
-
-fn print_player(player:Player) {
-	println!("{:?} has {:?} ballon d'ors", player.name, player.ballon_dors)
+struct Drinks {
+	flavor: Flavors,
+	ounce: f64
 }
 fn main() {
-	let the_best = Player {
-		name: Name::Messi,
-		ballon_dors: 8
+	let my_drink = Drinks {
+		flavor: Flavors::Apple,
+		ounce: 4.2
 	};
-	match the_best.name {
-		Name::Messi => println!("Messi is the best"),
-		Name::Maradona => println!("Maradona is the best")
-	}
-	print_player(the_best);
+
+	
+	print_drink(my_drink);
+
 }
+fn print_drink(drink: Drinks){
+	match drink.flavor {
+		Flavors::Chocolate => println!("This is a Chocolate drink"),
+		Flavors::Apple => println!("This is an Apple juice"),
+		Flavors::Orange => println!("Orange juice"),
+	}
+	println!("the ounce is {}", drink.ounce);
+}
+// #[derive(Debug)]
+// enum Name {
+// 	Messi,
+// 	Maradona
+// }
+// struct Player {
+// 	name: Name,
+// 	ballon_dors: i32
+// }
+
+// fn print_player(player:Player) {
+// 	println!("{:?} has {:?} ballon d'ors", player.name, player.ballon_dors)
+// }
+// fn main() {
+// 	let the_best = Player {
+// 		name: Name::Messi,
+// 		ballon_dors: 8
+// 	};
+// 	match the_best.name {
+// 		Name::Messi => println!("Messi is the best"),
+// 		Name::Maradona => println!("Maradona is the best")
+// 	}
+// 	print_player(the_best);
+// }
