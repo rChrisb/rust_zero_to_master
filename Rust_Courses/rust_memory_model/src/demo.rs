b901c65d9223dbb5a404cec4e7e2eb4bab24892e
rChrisb <5767@holbertonstudents.com>
2023-01-19 20:01:41 +0100

    working on implementation of struct

--- Rust_Courses/rust_memory_model/src/demo.rs
+++ Rust_Courses/rust_memory_model/src/demo.rs
@@ -13,6 +13,12 @@ struct Temperature {
 	degree: f64,
 }
 impl Temperature {
+	fn freezing()-> Self {
+		Self {
+			degree: -32.6
+		}
+	}
+
 	fn show_temperature(&self){
 		println!("It's {} degree F outside", self.degree);
 	}
@@ -23,7 +29,9 @@ fn main () {
 		degree : 77.8
 	};
 	temp.show_temperature();
+	let another_temp = Temperature::freezing();
 
+	another_temp.show_temperature();
 	/* let book = Book {
 		pages: 5,
 		rating: 9
