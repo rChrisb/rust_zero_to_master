eb29ec1f95af6aa50e12ee9d160f18e37ea87c61
rChrisb <5767@holbertonstudents.com>
2023-01-19 14:41:55 +0100

    Organizing similar data using structs

--- activities_Rust/src/bin/a8.rs
+++ activities_Rust/src/bin/a8.rs
@@ -9,31 +9,55 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
+// #[derive(Debug)]
+// enum Flavors {
+// 	Chocolate,
+// 	Apple,
+// 	Orange
+// }
+// struct Drinks {
+// 	flavor: Flavors,
+// 	ounce: f64
+// }
+// fn main() {
+// 	let my_drink = Drinks {
+// 		flavor: Flavors::Apple,
+// 		ounce: 4.2
+// 	};
+
+// 	match my_drink.flavor {
+// 		Flavors::Chocolate => println!("Chocolate drink"),
+// 		Flavors::Apple => println!("Apple juice"),
+// 		Flavors::Orange => println!("Orange juice"),
+// 	}
+// 	print_drink(my_drink);
+
+// }
+// fn print_drink(drink: Drinks){
+// 	println!("the flavor is {:?}", drink.flavor );
+// 	println!("the ounce is {}", drink.ounce);
+// }
 #[derive(Debug)]
-enum Flavors {
-	Chocolate,
-	Apple,
-	Orange
+enum Name {
+	Messi,
+	Maradona
 }
-struct Drinks {
-	flavor: Flavors,
-	ounce: f64
+struct Player {
+	name: Name,
+	ballon_dors: i32
+}
+
+fn print_player(player:Player) {
+	println!("{:?} has {:?} ballon d'ors", player.name, player.ballon_dors)
 }
 fn main() {
-	let my_drink = Drinks {
-		flavor: Flavors::Apple,
-		ounce: 4.2
+	let the_best = Player {
+		name: Name::Messi,
+		ballon_dors: 8
 	};
-
-	match my_drink.flavor {
-		Flavors::Chocolate => println!("Chocolate drink"),
-		Flavors::Apple => println!("Apple juice"),
-		Flavors::Orange => println!("Orange juice"),
+	match the_best.name {
+		Name::Messi => println!("Messi is the best"),
+		Name::Maradona => println!("Maradona is the best")
 	}
-	print_drink(my_drink);
-
-}
-fn print_drink(drink: Drinks){
-	println!("the flavor is {:?}", drink.flavor );
-	println!("the ounce is {}", drink.ounce);
+	print_player(the_best);
 }
