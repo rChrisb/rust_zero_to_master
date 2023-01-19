debedf7c38659724d00f055bf1de01173635af5b
rChrisb <5767@holbertonstudents.com>
2023-01-19 19:53:45 +0100

    working on implementation of struct

--- Rust_Courses/rust_memory_model/src/demo.rs
+++ Rust_Courses/rust_memory_model/src/demo.rs
@@ -1,20 +1,34 @@
-struct Book {
-	pages: i32,
-	rating:i32
-}
+// struct Book {
+// 	pages: i32,
+// 	rating:i32
+// }
 
-fn display_page_count(book:&Book){
-	println!("pages = {:?}", book.pages);
+// fn display_page_count(book:&Book){
+// 	println!("pages = {:?}", book.pages);
+// }
+// fn display_rating(book:&Book){
+// 	println!("rating = {:?}", book.rating);
+// }
+struct Temperature {
+	degree: f64,
 }
-fn display_rating(book:&Book){
-	println!("rating = {:?}", book.rating);
+impl Temperature {
+	fn show_temperature(&self){
+		println!("It's {} degree F outside", self.degree);
+	}
+	
 }
 fn main () {
-	let book = Book {
+	let temp = Temperature {
+		degree : 77.8
+	};
+	temp.show_temperature();
+
+	/* let book = Book {
 		pages: 5,
 		rating: 9
 	};
 	display_page_count(&book);
-	display_rating(&book);
+	display_rating(&book); */
 	
 }
