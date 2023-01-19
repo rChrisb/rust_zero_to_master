19ba6f1357e387dc5238f40e6368a9489ba72ac7
rChrisb <5767@holbertonstudents.com>
2023-01-19 14:31:46 +0100

    Organizing similar data using structs

---
+++ activities_Rust/src/bin/a8.rs
@@ -0,0 +1,39 @@
+// Topic: Organizing similar data using structs
+//
+// Requirements:
+// * Print the flavor of a drink and it's fluid ounces
+//
+// Notes:
+// * Use an enum to create different flavors of drinks
+// * Use a struct to store drink flavor and fluid ounce information
+// * Use a function to print out the drink flavor and ounces
+// * Use a match expression to print the drink flavor
+
+#[derive(Debug)]
+enum Flavors {
+	Chocolate,
+	Apple,
+	Orange
+}
+struct Drinks {
+	flavor: Flavors,
+	ounce: f64
+}
+fn main() {
+	let my_drink = Drinks {
+		flavor: Flavors::Apple,
+		ounce: 4.2
+	};
+
+	match my_drink.flavor {
+		Flavors::Chocolate => println!("Chocolate drink"),
+		Flavors::Apple => println!("Apple juice"),
+		Flavors::Orange => println!("Orange juice"),
+	}
+	print_drink(my_drink);
+
+}
+fn print_drink(drink: Drinks){
+	println!("the flavor is {:?}", drink.flavor );
+	println!("the ounce is {}", drink.ounce);
+}
