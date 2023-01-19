34b065890897aefb77d0116221c100352fc803e7
rChrisb <5767@holbertonstudents.com>
2023-01-19 13:01:31 +0100

    enum variables

---
+++ Rust_Fundamentals/working_with_data/src/demo.rs
@@ -0,0 +1,20 @@
+#[allow(dead_code)]
+enum Direction {
+	Left,
+	Right,
+	Down,
+}
+
+fn main () {
+	let go = Direction::Down;
+	// let mut go = Direction::Left;
+	// let mut go = Direction::Right;
+	// let gone = Direction::Right;
+	// let going = Direction::Left;
+
+	match go {
+		Direction::Left => println!("Go to the left"),
+		Direction::Right => println!("Go to the Right"),
+		Direction::Down => println!("Go Down"),
+	}
+}
