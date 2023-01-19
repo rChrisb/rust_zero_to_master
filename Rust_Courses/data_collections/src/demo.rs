dab83ba4b03700d201efb46b727a3a8a594d36ab
rChrisb <5767@holbertonstudents.com>
2023-01-19 20:58:49 +0100

    working on vectors and for loop

--- Rust_Courses/data_collections/src/demo.rs
+++ Rust_Courses/data_collections/src/demo.rs
@@ -13,8 +13,33 @@
 // 	}
 	
 // }
+struct Test {
+	score: i32
+}
 fn main () {
-	
+	let my_score = vec![
+		Test {score: 10},
+		Test {score: 15},
+		Test {score: 18},
+
+	];
+	let mut total = 0;
+	for test in &my_score {
+		total += test.score
+	}
+	println!("{}", my_score.len());
+	let average: f64 = (total / 3).into();
+	println!("{:?}", average);
+
+
+
+
+	// let numbers = vec![1, 2 , 3];
+	// for num in numbers {
+	// 	println!("{:?}", num);
+	// }
+
+
 
 
 
