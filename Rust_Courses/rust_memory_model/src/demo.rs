035037b83d497f18b3b135a232063ba55c6c924c
rChrisb <5767@holbertonstudents.com>
2023-01-19 20:39:52 +0100

    working on ownership

--- Rust_Courses/rust_memory_model/src/demo.rs
+++ Rust_Courses/rust_memory_model/src/demo.rs
@@ -9,29 +9,15 @@
 // fn display_rating(book:&Book){
 // 	println!("rating = {:?}", book.rating);
 // }
-struct Temperature {
-	degree: f64,
-}
-impl Temperature {
-	fn freezing()-> Self {
-		Self {
-			degree: -32.6
-		}
-	}
 
-	fn show_temperature(&self){
-		println!("It's {} degree F outside", self.degree);
-	}
-	
-}
 fn main () {
-	let temp = Temperature {
-		degree : 77.8
-	};
-	temp.show_temperature();
-	let another_temp = Temperature::freezing();
 
-	another_temp.show_temperature();
+
+
+
+	
+
+	// another_temp.show_temperature();
 	/* let book = Book {
 		pages: 5,
 		rating: 9
