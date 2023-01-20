10e10824474d07707f38cb49af750c8b386a4769
rChrisb <5767@holbertonstudents.com>
2023-01-20 18:39:58 +0100

    Result

---
+++ activities_Rust/src/bin/a18.rs
@@ -0,0 +1,47 @@
+  // Topic: Result
+//
+// Requirements:
+// * Create an structure named `Adult` that represents a person aged 21 or older:
+//   * The structure must contain the person's name and age
+//   * Implement Debug print functionality using `derive`
+// * Implement a `new` function for the `Adult` structure that returns a Result:
+//   * The Ok variant should contain the initialized structure, but only
+//     if the person is aged 21 or older
+//   * The Err variant should contain a String (or &str) that explains why
+//     the structure could not be created
+// * Instantiate two `Adult` structures:
+//   * One should be aged under 21
+//   * One should be 21 or over
+// * Use `match` to print out a message for each `Adult`:
+//   * For the Ok variant, print any message you want
+//   * For the Err variant, print out the error message
+
+
+#[derive(Debug)]
+struct Adult {
+	name: String,
+	age: i32
+}
+
+impl Adult {
+	fn create_person(name:&str, age: i32) -> Result<Self, String> {
+		if age >= 21 {
+			Ok(Self {
+				name: name.to_owned(),
+				age: age
+			})
+		} else {
+			Err("Under 21 years old".to_owned())
+		}
+	}
+}
+
+fn main() {
+	let person_1 = Adult::create_person("Rebecca", 41);
+	let person_2 = Adult::create_person("Camille", 15);
+
+	match person_1 {
+		Ok(result) => println!("{} is {}", result.name, result.age),
+		Err(result) => println!("{}", result)
+	}
+}
