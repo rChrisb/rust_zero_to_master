1b34a4052aac1c5b1ab57a1e715ebbfd9625b9f0
rChrisb <5767@holbertonstudents.com>
2023-01-21 14:52:44 +0100

    working on external crates

--- Rust_Courses/managing_code/src/demo.rs
+++ Rust_Courses/managing_code/src/demo.rs
@@ -1,15 +1,22 @@
-fn all_caps(word: &str) -> String {
-    word.to_uppercase()
-}
-fn main() {}
-#[cfg(test)]
-mod test {
-    use crate::*;
+// fn all_caps(word: &str) -> String {
+//     word.to_uppercase()
+// }
+// extern crate humantime;
+// use humantime::format_duration;
+// use std::time::Duration;
 
-    #[test]
-    fn check_all_caps() {
-        let result = all_caps("bonjour");
-        let expected = "BONJOUR".to_owned();
-        assert_eq!(result, expected, "It should be all uppercase")
-    }
+fn main() {
+    // let d = Duration::from_secs(9876);
+    // println!("{}", format_duration(d));
 }
+// #[cfg(test)]
+// mod test {
+//     use crate::*;
+
+//     #[test]
+//     fn check_all_caps() {
+//         let result = all_caps("bonjour");
+//         let expected = "BONJOUR".to_owned();
+//         assert_eq!(result, expected, "It should be all uppercase")
+//     }
+// }
