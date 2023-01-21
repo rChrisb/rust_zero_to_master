d1b6e17f08c6b6e9537b0a1f2f5dc5c09a718e24
rChrisb <5767@holbertonstudents.com>
2023-01-21 12:10:22 +0100

    working on iterators functions

--- Rust_Courses/easier_data_management/src/demo.rs
+++ Rust_Courses/easier_data_management/src/demo.rs
@@ -3,30 +3,43 @@
 // }
 // fn maybe_num() -> Option<i32> {...}
 // fn maybe_word() -> Option<String> {...}
-fn main () {
-	
-	let a: Option<i32> = Some(7);
+fn main() {
+    let numbers = vec![1, 2, 3, 4];
+
+    /*   let mut plus_one = vec![];
+    for num in numbers {
+        plus_one.push(num + 1);
+    } */
+    let plus_one: Vec<_> = numbers
+        .iter()
+        .map(|num| num + 1)
+        .filter(|num| num % 2 == 0)
+        /*    .last(); */
+        /* .take(1) */
+        .collect();
+    println!("{:?}", plus_one)
+
+    /* let a: Option<i32> = Some(7);
 	let a_is_some = a.is_some();
 	let a_is_none = a.is_none();
 	let a_mapped = a.map(|num| num + 1);
 	let a_or_else = a.or_else(|| Some(10));
 	let unwrapped = a.unwrap_or_else(|| 0);
-	let a_filtered = a.filter(|num| num == &4);
+	let a_filtered = a.filter(|num| num == &4); */
 
-	
-	// let plus_one = match maybe_num() {
-	// 	Some(num) => Some(nu + 1),
-	// 	None => None
-	// };
-	// let pluss__one = maybe_num.map(|num| num + 1);
-	// let world_length = maybe_word.map(|word| word.len());
-	// let sum_1 = add(3,5);
-	// println!("{sum_1}");
+    // let plus_one = match maybe_num() {
+    // 	Some(num) => Some(nu + 1),
+    // 	None => None
+    // };
+    // let pluss__one = maybe_num.map(|num| num + 1);
+    // let world_length = maybe_word.map(|word| word.len());
+    // let sum_1 = add(3,5);
+    // println!("{sum_1}");
 
-	// let mul = |a:i32, b:i32 | ->i32 {
-	// 	a * b
-	// };
-	// let div = |a, b| a/b;
-	// println!("{:?}", mul(5, 4));
-	// println!("{:?}", div(8, 2));
+    // let mul = |a:i32, b:i32 | ->i32 {
+    // 	a * b
+    // };
+    // let div = |a, b| a/b;
+    // println!("{:?}", mul(5, 4));
+    // println!("{:?}", div(8, 2));
 }
