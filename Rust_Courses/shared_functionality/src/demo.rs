1b1112440157d23a089ac416dd009272298f2cee
rChrisb <5767@holbertonstudents.com>
2023-01-25 12:27:19 +0100

    working on generic functions

--- Rust_Courses/shared_functionality/src/demo.rs
+++ Rust_Courses/shared_functionality/src/demo.rs
@@ -1,25 +1,66 @@
-trait Fall {
-    fn hit_ground(&self);
+// trait Fall {
+//     fn hit_ground(&self);
+// }
+// struct Vase;
+// impl Fall for Vase {
+//     fn hit_ground(&self) {
+//         println!("the vase broke")
+//     }
+// }
+// struct Cat;
+
+// impl Fall for Cat {
+//     fn hit_ground(&self) {
+//         println!("the cat walked away")
+//     }
+// }
+
+// fn fall(thing: impl Fall) {
+//     thing.hit_ground()
+// }
+trait CheckIn {
+    fn check_in(&self);
+    fn process(&self);
 }
-struct Vase;
-impl Fall for Vase {
-    fn hit_ground(&self) {
-        println!("the vase broke")
+fn process_item<T: CheckIn>(item: T) {
+    item.check_in();
+    item.process()
+}
+
+struct Pilot;
+impl CheckIn for Pilot {
+    fn check_in(&self) {
+        println!("checked in as pilot")
+    }
+    fn process(&self) {
+        println!("Pilot enters the cockpit");
     }
 }
-struct Cat;
-
-impl Fall for Cat {
-    fn hit_ground(&self) {
-        println!("the cat walked away")
+struct Passenger;
+impl CheckIn for Passenger {
+    fn check_in(&self) {
+        println!("checked in as passenger")
+    }
+    fn process(&self) {
+        println!("passenger takes a seat");
     }
 }
-
-fn fall(thing: impl Fall) {
-    thing.hit_ground()
+struct Cargo;
+impl CheckIn for Cargo {
+    fn check_in(&self) {
+        println!("cargo checked in")
+    }
+    fn process(&self) {
+        println!("cargo moved to storage");
+    }
 }
-
 fn main() {
-    fall(Vase);
-    fall(Cat)
+    let paul = Pilot;
+    let matthiew = Passenger;
+    let khloe = Cargo;
+    process_item(paul);
+    process_item(matthiew);
+    process_item(khloe)
+    // fall(Vase);
+    // fall(Cat)
 }
