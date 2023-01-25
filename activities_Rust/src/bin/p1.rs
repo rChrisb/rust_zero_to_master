5cd30973f0061d8cba17f1293af6fa7e3f38c1da
rChrisb <5767@holbertonstudents.com>
2023-01-25 09:44:35 +0100

    Interactive bill manager

--- activities_Rust/src/bin/p1.rs
+++ activities_Rust/src/bin/p1.rs
@@ -35,55 +35,168 @@ enum MenuAction {
     Update,
     GoBack,
 }
+use std::io;
+
+fn get_input() -> Option<String> {
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer).is_err() {
+        println!("Please type your data again");
+    }
+    let input = buffer.trim().to_owned();
+    if input == "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+fn get_bill_amount() -> Option<f64> {
+    println!("Amount:");
+    loop {
+        let input = match get_input() {
+            Some(input) => input,
+            None => {
+                return None;
+            }
+        };
+        if input == "" {
+            return None;
+        }
+        let parsed_input: Result<f64, _> = input.parse();
+        match parsed_input {
+            Ok(amount) => {
+                return Some(amount);
+            }
+            Err(_) => println!("Please enter a number"),
+        }
+    }
+}
+impl MenuAction {
+    fn show() {
+        println!(
+            "\n =====BILL MANAGER=====\n\n1. Add a new Bill\n2. Views your Bills\n3. Remove a Bill\n4. Edit a Bill\n\nEnter your choice"
+        )
+    }
+    fn from_str(input: &str) -> Option<MenuAction> {
+        match input {
+            "1" => Some(Self::Add),
+            "2" => Some(Self::View),
+            "3" => Some(Self::Remove),
+            "4" => Some(Self::Update),
+            _ => None,
+        }
+    }
+}
 
 #[derive(Debug)]
 #[derive(Clone)]
-struct Bill {
+pub struct Bill {
     name: String,
-    amount_owed: f64,
-}
-fn add_bill(name: &str, amount: f64, mut bills: Vec<Bill>) -> Vec<Bill> {
-    let new_bill = Bill {
-        name: name.to_owned(),
-        amount_owed: amount,
-    };
-    bills.push(new_bill);
-    println!("----------Bills have been updated------\n");
-    return bills;
+    amount: f64,
 }
 
-fn remove_bill(name: &str, amount: f64, mut bills: Vec<Bill>) -> Vec<Bill> {
-    bills.retain(|bill| bill.name != name && bill.amount_owed != amount);
-    println!("----------Bills have been updated------\n");
-    return bills;
+pub struct Bills {
+    inner: Vec<Bill>,
 }
-fn edit_bill(
-    mut bills: Vec<Bill>,
-    name: &str,
-    amount: f64,
-    new_name: &str,
-    new_amount: f64
-) -> Vec<Bill> {
-    for bill in bills.iter_mut() {
-        if bill.name == name && bill.amount_owed == amount {
-            bill.name = new_name.to_owned();
-            bill.amount_owed = new_amount;
+
+impl Bills {
+    fn new() -> Self {
+        Self {
+            inner: vec![],
         }
     }
-    println!("----------Bills have been updated------\n");
-    return bills;
-}
 
-fn see_bills(bills: &Vec<Bill>) {
-    let mut bill_number = 1;
-    for bill in bills {
-        println!("Bill#{:?}\nname:{:?}, amount:{:?}\n\n", bill_number, bill.name, bill.amount_owed);
-        bill_number += 1;
+    fn add(&mut self, bill: Bill) {
+        self.inner.push(bill);
+    }
+
+    fn get_all(&self) -> Vec<&Bill> {
+        self.inner.iter().collect()
+    }
+}
+mod menu {
+    use crate::{ Bill, Bills, get_input, get_bill_amount };
+    pub fn add_bill(bills: &mut Bills) {
+        println!("Bill name:");
+        let name = match get_input() {
+            Some(input) => input,
+            None => {
+                return;
+            }
+        };
+        let amount = match get_bill_amount() {
+            Some(amount) => amount,
+            None => {
+                return;
+            }
+        };
+        let bill = Bill {
+            name,
+            amount,
+        };
+        bills.add(bill);
+        println!("Bill has been added")
+    }
+    pub fn view_all_bills(bills: &Bills) {
+        for bill in bills.get_all() {
+            println!("{:?}", bill);
+        }
     }
 }
 
+// pub fn remove_bill(name: &str, amount: f64, mut bills: Vec<Bill>) -> Vec<Bill> {
+//     bills.retain(|bill| bill.name != name && bill.amount_owed != amount);
+//     println!("----------Bills have been updated------\n");
+//     return bills;
+// }
+// pub fn edit_bill(
+//     mut bills: Vec<Bill>,
+//     name: &str,
+//     amount: f64,
+//     new_name: &str,
+//     new_amount: f64
+// ) -> Vec<Bill> {
+//     for bill in bills.iter_mut() {
+//         if bill.name == name && bill.amount_owed == amount {
+//             bill.name = new_name.to_owned();
+//             bill.amount_owed = new_amount;
+//         }
+//     }
+//     println!("----------Bills have been updated------\n");
+//     return bills;
+// }
+
+// pub fn see_bills(bills: &Vec<Bill>) {
+//     let mut bill_number = 1;
+//     for bill in bills {
+//         println!(
+//             "Bill#{:?}\nname:{:?}, amount:{:?}\n\n",
+//             bill_number,
+//             bill.name,
+//             bill.amount_owed
+//         );
+//         bill_number += 1;
+//     }
+// }
+
 fn main() {
-    let mut bills: Vec<Bill> = Vec::new();
+    let mut bills = Bills::new();
+    loop {
+        MenuAction::show();
+        let input = get_input().expect("no data entered");
+        match MenuAction::from_str(input.as_str()) {
+            Some(MenuAction::Add) => menu::add_bill(&mut bills),
+            Some(MenuAction::View) => menu::view_all_bills(&bills),
+            Some(MenuAction::Remove) => (),
+            Some(MenuAction::Update) => (),
+            Some(MenuAction::GoBack) => (),
+
+            None => {
+                return;
+            }
+        }
+    }
+
+    /* let mut bills: Vec<Bill> = Vec::new();
     bills = add_bill("Victor", 2000.0, bills);
     bills = add_bill("Hector", 452.23, bills);
     bills = add_bill("Chris", 1200.25, bills);
@@ -94,5 +207,5 @@ fn main() {
     see_bills(&bills);
     bills = add_bill("Rubie", 45200.25, bills);
     bills = add_bill("Bikoy", 3455.5, bills);
-    see_bills(&bills)
+    see_bills(&bills) */
 }
