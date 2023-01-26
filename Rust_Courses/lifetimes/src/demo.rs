4878cfc3e23e491fd3141b6a2ce8480f849a0bbd
rChrisb <5767@holbertonstudents.com>
2023-01-26 12:25:53 +0100

    working on lifetimes

---
+++ Rust_Courses/lifetimes/src/demo.rs
@@ -0,0 +1,77 @@
+#[derive(Debug)]
+struct Cards {
+    inner: Vec<IdCard>,
+}
+
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
+enum City {
+    Barland,
+    Bazopolis,
+    Fooville,
+}
+#[allow(dead_code)]
+#[derive(Debug)]
+struct IdCard {
+    name: String,
+    age: u8,
+    city: City,
+}
+
+impl IdCard {
+    pub fn new(name: &str, age: u8, city: City) -> Self {
+        Self {
+            name: name.to_string(),
+            age,
+            city,
+        }
+    }
+}
+
+fn new_ids() -> Cards {
+    Cards {
+        inner: vec![
+            IdCard::new("Amy", 1, City::Fooville),
+            IdCard::new("Matt", 10, City::Barland),
+            IdCard::new("Bailee", 20, City::Barland),
+            IdCard::new("Anthony", 30, City::Bazopolis),
+            IdCard::new("Tina", 40, City::Bazopolis)
+        ],
+    }
+}
+
+#[derive(Debug)]
+struct YoungPeople<'a> {
+    inner: Vec<&'a IdCard>,
+}
+impl<'a> YoungPeople<'a> {
+    fn living_in_fooville(&self) -> Self {
+        Self {
+            inner: self.inner
+                .iter()
+                .filter(|id| id.city == City::Fooville)
+                .map(|id| *id)
+                .collect(),
+        }
+    }
+}
+fn main() {
+    let ids = new_ids();
+    let young = YoungPeople {
+        inner: ids.inner
+            .iter()
+            .filter(|id_card| id_card.age <= 20)
+            .collect(),
+    };
+    println!("all ids:");
+    for id in ids.inner.iter() {
+        println!("{:?}", id);
+    }
+    println!("\nyoung:");
+    for id in young.inner.iter() {
+        println!("{:?}", id);
+    }
+    println!("\nyoung people living in foolville:");
+    for id in young.living_in_fooville().inner.iter() {
+        println!("{:?}", id);
+    }
+}
