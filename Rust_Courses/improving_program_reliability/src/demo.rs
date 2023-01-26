b30d47d573133ed134cb285ab8609a5ce8015c06
rChrisb <5767@holbertonstudents.com>
2023-01-26 15:18:56 +0100

    working on custom error types

--- Rust_Courses/improving_program_reliability/src/demo.rs
+++ Rust_Courses/improving_program_reliability/src/demo.rs
@@ -1,44 +1,59 @@
-use chrono::{ DateTime, Duration, Utc };
-use thiserror::Error;
+// use chrono::{ DateTime, Duration, Utc };
+// use thiserror::Error;
 
-struct SubwayPass {
-    id: usize,
-    funds: isize,
-    expires: DateTime<Utc>,
-}
-#[derive(Debug, Error)]
-enum PassError {
-    #[error("expired pass")]
-    PassExpired,
-    #[error("insufficient funds: {0}")] InsufficientFunds(isize),
-    #[error("pass read error")] ReadError(String),
-}
+// struct SubwayPass {
+//     id: usize,
+//     funds: isize,
+//     expires: DateTime<Utc>,
+// }
+// #[derive(Debug, Error)]
+// enum PassError {
+//     #[error("expired pass")]
+//     PassExpired,
+//     #[error("insufficient funds: {0}")] InsufficientFunds(isize),
+//     #[error("pass read error")] ReadError(String),
+// }
 
-fn swipe_card() -> Result<SubwayPass, PassError> {
-    Ok(SubwayPass {
-        id: 0,
-        funds: 200,
-        expires: Utc::now() + Duration::weeks(52),
-    })
-}
+// fn swipe_card() -> Result<SubwayPass, PassError> {
+//     Ok(SubwayPass {
+//         id: 0,
+//         funds: 200,
+//         expires: Utc::now() + Duration::weeks(52),
+//     })
+// }
 
-fn use_pass(pass: &mut SubwayPass, cost: isize) -> Result<(), PassError> {
-    if Utc::now() > pass.expired {
-        Err(PassError::PassExpired)
-    } else {
-        if pass.funds - cost < 0 {
-            Err(PassError::InsufficientFunds(pass.funds))
-        } else {
-            pass.funds = pass.funds - cost;
-            Ok(())
-        }
+// fn use_pass(pass: &mut SubwayPass, cost: isize) -> Result<(), PassError> {
+//     if Utc::now() > pass.expired {
+//         Err(PassError::PassExpired)
+//     } else {
+//         if pass.funds - cost < 0 {
+//             Err(PassError::InsufficientFunds(pass.funds))
+//         } else {
+//             pass.funds = pass.funds - cost;
+//             Ok(())
+//         }
+//     }
+// }
+#[derive(Debug, Clone, Copy)]
+struct NeverZero(i32);
+impl NeverZero {
+    fn new(i: i32) -> Result<Self, String> {
+        if i == 0 { Err("can NOT be zero".to_owned()) } else { Ok(Self(i)) }
     }
+}
+fn divide(a: i32, b: NeverZero) -> i32 {
+    let b = b.0;
+    a / b
 }
 
 fn main() {
-    let pass_status = swipe_card().and_then(|mut pass| use_pass(&mut pass, 3));
-    match pass_status {
-        Ok(_) => println!("Ok to board!"),
-        Err(e) => println!("error: {}", e),
+    match NeverZero::new(10) {
+        Ok(nevzero) => println!("{:?}", divide(10, nevzero)),
+        Err(e) => println!("{:?}", e),
     }
+    // let pass_status = swipe_card().and_then(|mut pass| use_pass(&mut pass, 3));
+    // match pass_status {
+    //     Ok(_) => println!("Ok to board!"),
+    //     Err(e) => println!("error: {}", e),
+    // }
 }
