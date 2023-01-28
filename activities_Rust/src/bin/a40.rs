615ab1e4e8c0c6377172e66ef7f20372aac07a36
rChrisb <5767@holbertonstudents.com>
2023-01-28 16:53:28 +0100

    Smart Pointers & RefCell

--- activities_Rust/src/bin/a40.rs
+++ activities_Rust/src/bin/a40.rs
@@ -20,9 +20,74 @@
 //   can both access the rental information
 // * Test your program by changing the vehicle status from both a storefront
 //   and from corporate
+use std::cell::RefCell;
+use std::rc::Rc;
 
-struct Corporate;
+#[derive(Debug)]
+enum Vehicle {
+    Car,
+    Truck,
+}
+#[derive(Debug, Hash, PartialOrd, PartialEq)]
+enum Status {
+    Available,
+    Unavailable,
+    Rented,
+    Maintenance,
+}
 
-struct StoreFront;
+#[derive(Debug)]
+struct Rental {
+    status: Status,
+    vehicle: Vehicle,
+    vin: String,
+}
+
+struct Corporate(Rc<RefCell<Vec<Rental>>>);
+
+struct StoreFront(Rc<RefCell<Vec<Rental>>>);
 
 fn main() {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn update_status() {
+        let vehicles = vec![
+            Rental {
+                status: Status::Available,
+                vehicle: Vehicle::Car,
+                vin: "123".to_owned(),
+            },
+            Rental {
+                status: Status::Maintenance,
+                vehicle: Vehicle::Truck,
+                vin: "abc".to_owned(),
+            }
+        ];
+        let vehicles = Rc::new(RefCell::new(vehicles));
+        let corporate = Corporate(Rc::clone(&vehicles));
+        let storefront = StoreFront(Rc::clone(&vehicles));
+
+        {
+            let mut rentals = storefront.0.borrow_mut();
+            if let Some(car) = rentals.get_mut(0) {
+                assert_eq!(car.status, Status::Available);
+                car.status = Status::Rented;
+            }
+        }
+        {
+            let mut rentals = corporate.0.borrow_mut();
+            if let Some(car) = rentals.get_mut(0) {
+                assert_eq!(car.status, Status::Rented);
+                car.status = Status::Available;
+            }
+        }
+        let rentals = storefront.0.borrow();
+        if let Some(car) = rentals.get(0) {
+            assert_eq!(car.status, Status::Available);
+        }
+    }
+}
