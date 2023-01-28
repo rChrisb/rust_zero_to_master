a0f9ba7ba0679d1e6f3ae23116554619acf06f15
rChrisb <5767@holbertonstudents.com>
2023-01-28 13:34:37 +0100

    working on channels

--- Rust_Courses/parallel_execution/src/demo.rs
+++ Rust_Courses/parallel_execution/src/demo.rs
@@ -1,35 +1,52 @@
 use crossbeam_channel::unbounded;
 use std::thread;
 
-enum ThreadMsg {
+enum WorkerMsg {
     PrintData(String),
     Sum(i64, i64),
     Quit,
 }
+enum MainMsg {
+    SumResult(i64),
+    WorkerQuit,
+}
 fn main() {
-    let (sender, receiver) = unbounded();
-    let handle = thread::spawn(move || {
+    let (worker_tx, receiver_worker) = unbounded();
+    let (main_tx, receiver_main) = unbounded();
+    let worker = thread::spawn(move || {
         loop {
-            match receiver.recv() {
+            match receiver_worker.recv() {
                 Ok(msg) =>
                     match msg {
-                        ThreadMsg::PrintData(msg) => println!("{}", msg),
-                        ThreadMsg::Sum(x, y) => println!("{} + {} = {}", x, y, x + y),
-                        ThreadMsg::Quit => {
-                            println!("Disconnecting");
+                        WorkerMsg::PrintData(msg) => println!("Worker:{}", msg),
+                        WorkerMsg::Sum(x, y) => {
+                            println!("Worker: summing...");
+                            main_tx.send(MainMsg::SumResult(x + y));
+                        }
+                        WorkerMsg::Quit => {
+                            println!("end of thread by Worker");
+                            main_tx.send(MainMsg::WorkerQuit);
                             break;
                         }
                     }
                 Err(msg) => {
-                    println!("Disconnecting");
+                    println!("Worker Disconnecting");
+                    main_tx.try_send(MainMsg::WorkerQuit);
                     break;
                 }
             }
         }
     });
-    sender.send(ThreadMsg::PrintData("Hello World!".to_owned()));
-    sender.send(ThreadMsg::Sum(7, 8));
-    sender.send(ThreadMsg::Quit);
+    worker_tx.send(WorkerMsg::PrintData("Hello World!".to_owned()));
+    worker_tx.send(WorkerMsg::Sum(7, 8));
+    worker_tx.send(WorkerMsg::Quit);
+
+    while let Ok(msg) = receiver_main.recv() {
+        match msg {
+            MainMsg::SumResult(answer) => println!("Main answer: {}", answer),
+            MainMsg::WorkerQuit => println!("Main: worker terminated"),
+        }
+    }
 
     handle.join();
     // let data = vec!['a', 'b', 'c', 'd'];
