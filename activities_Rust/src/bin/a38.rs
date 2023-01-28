82a033a234603cd80a13d519e03adc34933e7913
rChrisb <5767@holbertonstudents.com>
2023-01-28 12:25:04 +0100

    Multithreading

--- activities_Rust/src/bin/a38.rs
+++ activities_Rust/src/bin/a38.rs
@@ -7,7 +7,7 @@
 //
 // Notes:
 // * Use the join function to wait for threads to finish
-
+use std::thread;
 fn msg_hello() -> &'static str {
     use std::time::Duration;
     std::thread::sleep(Duration::from_millis(1000));
@@ -26,4 +26,28 @@ fn msg_excited() -> &'static str {
     "!"
 }
 
-fn main() {}
+fn main() {
+    let a = thread::spawn(move || { msg_hello() });
+    let mut message = "".to_owned();
+    match a.join() {
+        Ok(msg) => {
+            message += msg;
+        }
+        _ => println!("error"),
+    }
+    let b = thread::spawn(move || { msg_thread() });
+    match b.join() {
+        Ok(msg) => {
+            message += msg;
+        }
+        _ => println!("error"),
+    }
+    let c = thread::spawn(move || { msg_excited() });
+    match c.join() {
+        Ok(msg) => {
+            message += msg;
+        }
+        _ => println!("error"),
+    }
+    println!("{}", message)
+}
