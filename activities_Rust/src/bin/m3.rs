968bb54c657d4f81829e1fc49250c0b2ca3abf32
rChrisb <5767@holbertonstudents.com>
2023-01-29 13:09:50 +0100

    Basic macro repetitions (generate hashmaps)

--- activities_Rust/src/bin/m3.rs
+++ activities_Rust/src/bin/m3.rs
@@ -4,5 +4,25 @@
 //   * Create a macro to generate hashmaps.
 //   * The macro must be able to accept multiple key/value pairs.
 //   * Print out the generated hashmap using the `dbg!` macro to ensure it works.
-
-fn main() {}
+macro_rules! hashmap {
+    ($ ($key:tt: $value:tt), + $(,)?) => {
+		{
+			let mut hm = std::collections::HashMap::new();
+			$(
+				hm.insert($key, $value);
+			)+
+			hm
+		}
+    };
+}
+fn main() {
+    let hashmap = hashmap![1: "a", 2: "b", 3: "c"];
+    // let hashmap = {
+    //     let mut hashmap = std::collections::HashMap::new();
+    //     hashmap.insert(1, "a");
+    //     hashmap.insert(2, "b");
+    //     hashmap.insert(3, "c");
+    //     hashmap
+    // };
+    dbg!(hashmap);
+}
