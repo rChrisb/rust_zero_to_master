cb35ff6071b532e5cc12a511bf6ddf733b541273
rChrisb <5767@holbertonstudents.com>
2023-02-02 10:12:42 +0100

    Implement Database Wrapper

--- clipstash/src/lib/data/mod.rs
+++ clipstash/src/lib/data/mod.rs
@@ -2,10 +2,11 @@ use derive_more::{ Display, From };
 use serde::{ Deserialize, Serialize };
 use std::str::FromStr;
 use uuid::Uuid;
+use sqlx::Sqlite;
 
 #[derive(Debug, thiserror::Error)]
 pub enum DataError {
-    #[error(database error: {0})] Database(#[from] sqlx::Error),
+    #[error("database error: {0}")] Database(#[from] sqlx::Error),
 }
 
 pub type AppDatabase = Database<Sqlite>;
@@ -13,6 +14,27 @@ pub type DatabasePool = sqlx::sqlite::SqlitePool;
 pub type Transaction<'t> = sqlx::Transaction<'t, Sqlite>;
 pub type AppDatabaseRow = sqlx::sqlite::SqliteRow;
 pub type AppQueryResult = sqlx::sqlite::SqliteQueryResult;
+
+pub struct Database<D: sqlx::Database>(sqlx::Pool<D>);
+
+impl Database<Sqlite> {
+    pub async fn new(connection_str: &str) -> Self {
+        let pool = sqlx::sqlite::SqlitePoolOptions::new().connect(connection_str).await;
+        match pool {
+            Ok(pool) => Self(pool),
+            Err(e) => {
+                eprintln!("{}\n", e);
+                eprintln!(
+                    "If the database has not yet been created, run: \n    $ sqlx database setup\n"
+                );
+                panic!("database connection error");
+            }
+        }
+    }
+    pub fn get_pool(&self) -> &DatabasePool {
+        &self.0
+    }
+}
 
 #[derive(Clone, Debug, Deserialize, Serialize, From, Display)]
 pub struct DbId(Uuid);
