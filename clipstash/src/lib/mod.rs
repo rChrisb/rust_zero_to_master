097aa3a20dbb386c4552637b3397171ed6a65a9f
rChrisb <5767@holbertonstudents.com>
2023-02-02 10:47:47 +0100

    Add modules to be use by database

--- clipstash/src/lib/mod.rs
+++ clipstash/src/lib/mod.rs
@@ -3,6 +3,6 @@ pub mod domain;
 pub mod service;
 pub mod web;
 
-pub use domain::clip::field::Shortcode;
+pub use domain::clip::field::ShortCode;
 pub use domain::clip::ClipError;
 pub use domain::time::Time;
