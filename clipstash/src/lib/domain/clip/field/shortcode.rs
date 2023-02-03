4a861f5ad1d6b824accbd196c0589b10c965ed28
rChrisb <5767@holbertonstudents.com>
2023-02-03 16:06:49 +0100

    Implement FromParam for Shortcode

--- clipstash/src/lib/domain/clip/field/shortcode.rs
+++ clipstash/src/lib/domain/clip/field/shortcode.rs
@@ -44,6 +44,15 @@ impl From<&str> for ShortCode {
         ShortCode(shortcode.to_owned())
     }
 }
+use rocket::request::FromParam;
+
+impl<'r> FromParam<'r> for ShortCode {
+    type Error = &'r str;
+
+    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
+        Ok(ShortCode::from(param))
+    }
+}
 
 impl FromStr for ShortCode {
     type Err = ClipError;
