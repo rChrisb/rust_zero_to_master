bf50d27eed9a59d94168c97f493d6535f436cc66
rChrisb <5767@holbertonstudents.com>
2023-02-04 18:04:58 +0100

    API: Routing

--- clipstash/src/lib/domain/clip/mod.rs
+++ clipstash/src/lib/domain/clip/mod.rs
@@ -11,11 +11,12 @@ pub enum ClipError {
     EmptyContent,
     #[error("invalid date: {0}")] InvalidDate(String),
     #[error("date parse error: {0}")] DateParse(#[from] chrono::ParseError),
-    #[error("id parse error: {0}")] Id(#[from] unid::Error),
+    #[error("id parse error: {0}")] Id(#[from] uuid::Error),
     #[error("hits parse error: {0}")] Hits(#[from] std::num::TryFromIntError),
 }
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Clip {
+    #[serde(skip)]
     pub clip_id: field::ClipId,
     pub shortcode: field::ShortCode,
     pub content: field::Content,
