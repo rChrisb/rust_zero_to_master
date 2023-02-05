120bdc3a08392a92ace54375506b6f7791fca803
rChrisb <5767@holbertonstudents.com>
2023-02-05 09:17:10 +0100

    Maintenance Tasks

--- clipstash/src/lib/service/action.rs
+++ clipstash/src/lib/service/action.rs
@@ -55,6 +55,6 @@ pub async fn api_key_is_valid(api_key: ApiKey, pool: &DatabasePool) -> Result<bo
     Ok(query::api_key_is_valid(api_key, pool).await?)
 }
 
-pub async fn delete_expires(pool: &DatabasePool) -> Result<u64, ServiceError> {
+pub async fn delete_expired(pool: &DatabasePool) -> Result<u64, ServiceError> {
     Ok(query::delete_expired(pool).await?)
 }
