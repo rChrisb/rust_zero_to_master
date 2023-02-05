864ea736a97a46e8dbb715e2ac0aaa6e1d0e0b9f
rChrisb <5767@holbertonstudents.com>
2023-02-05 09:13:41 +0100

    Maintenance Tasks

--- clipstash/src/lib/mod.rs
+++ clipstash/src/lib/mod.rs
@@ -14,6 +14,7 @@ use rocket::fs::FileServer;
 use rocket::{ Build, Rocket };
 use web::{ renderer::Renderer };
 use web::hitcounter::HitCounter;
+use domain::maintenance:Maintenance;
 
 pub fn rocket(config: RocketConfig) -> Rocket<Build> {
     rocket
@@ -21,6 +22,7 @@ pub fn rocket(config: RocketConfig) -> Rocket<Build> {
         .manage::<AppDatabase>(config.database)
         .manage::<Renderer>(config.renderer)
         .manage::<HitCounter>(config.hit_counter)
+        .manage::<Maintenance>(config.maintenance)
         .mount("/", web::http::routes())
         .mount("/api/clip", web::api::routes())
         .mount("/static", FileServer::from("static"))
@@ -31,4 +33,5 @@ pub struct RocketConfig {
     pub renderer: Renderer<'static>,
     pub database: AppDatabase,
     pub hit_counter: HitCounter,
+    pub maintenance: Maintenance,
 }
